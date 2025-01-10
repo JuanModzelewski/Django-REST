e612b678a64dfbaf39127cf86a1813b82994e620
JuanModzelewski <juanmodzelewski@gmail.com>
2025-01-10 18:38:14 +0100

    change allowed hosted to accept heroku app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'django-restframework-ap.herokuapp.com']
 
 
 # Application definition
