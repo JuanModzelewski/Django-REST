f12cf394a5b9e94a517f9b8126e3e9efe036e81d
JuanModzelewski <juanmodzelewski@gmail.com>
2025-01-14 18:19:26 +0100

    Updates to check auth

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -53,10 +53,10 @@ if 'DEV' not in os.environ:
     ]
 
 
-REST_USE_JWT = True
+USE_JWT = True
 JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_COOKIE = 'jwt-auth'
+JWT_AUTH_REFRESH_COOKIE = 'jwt-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 
